930ed4b0d4c6190970d10d2f4b7d81e40244d9fb
timukh <timukh@gmail.com>
2020-03-28 23:52:59 +0530

    added counter

--- app.js
+++ app.js
@@ -111,3 +111,18 @@ function playSound(){
 function winner(){
     new Audio('winner.wav').play();
 }
+
+
+
+
+
+
+// counter
+var n = localStorage.getItem('on_load_counter');
+
+    if (n === null) {
+        n = 0;
+    }
+    n++;
+    localStorage.setItem("on_load_counter", n);
+    document.getElementById('CounterVisitor').innerHTML = n;
